9c1ff1fcff3e053f70213cafdf603ccd2fd8b8f0
multida <wow_da65@naver.com>
2023-02-18 22:25:09 +0900

    🚧 변수, 상수, 문자열

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('This works!');
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
+
+//userInput
