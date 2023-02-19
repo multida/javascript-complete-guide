f67b885779c6c3c88686c6776b886fe3c485be8e
multida <wow_da65@naver.com>
2023-02-19 16:00:34 +0900

    🔨 문자열을 숫자로 변환하는 방법

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -2,7 +2,15 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + +userInput.value;
+  /* 
+    value를 받아오면 문자열로 오기 때문에 숫자로 바꿔줘야 한다.
+    1. parseInt/parseFloat -> parseInt(userInput.value)
+    2. + -> 문자열 앞에 +가 붙으면 숫자로 전환 가능함.
+
+    숫자를 문자열로 변환해주는 것 .toString
+    currentResult.toString()
+  */
   outputResult(currentResult, "");
 }
 
