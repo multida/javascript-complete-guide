85d18caa4bb8198f88a324ad59fd122902ea6c01
multida <wow_da65@naver.com>
2023-02-20 23:47:11 +0900

    ♻️ 각 연산자 함수를 리팩토링 해보았다.

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -6,10 +6,18 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+//중복되는 코드를 따로 함수로 뺌
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+
+  outputResult(currentResult, calcDescription);
+}
+
 function add() {
   const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
   /* 
     value를 받아오면 문자열로 오기 때문에 숫자로 바꿔줘야 한다.
     1. parseInt/parseFloat -> parseInt(userInput.value)
@@ -18,23 +26,33 @@ function add() {
     숫자를 문자열로 변환해주는 것 .toString
     currentResult.toString()
   */
-  outputResult(currentResult, calcDescription);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
 
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+
+  createAndWriteOutput("*", initialResult, enteredNumber);
 }
 
-function divide() {}
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+
+  createAndWriteOutput("/", initialResult, enteredNumber);
+}
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
