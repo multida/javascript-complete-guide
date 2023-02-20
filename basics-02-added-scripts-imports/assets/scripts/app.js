bcb5dd80a13bf4af95d9135f5dcb7816c898fbca
multida <wow_da65@naver.com>
2023-02-20 23:38:24 +0900

    🚧

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -21,4 +21,20 @@ function add() {
   outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+}
+
+function divide() {}
+
 addBtn.addEventListener("click", add);
